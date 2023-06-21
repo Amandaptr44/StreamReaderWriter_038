03ca59497441dd55db03768b09214906a2f78bb9
Amandaptr44 <amandaptr86@gmail.com>
2023-06-21 16:03:52 +0700

    penanda 2 bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan

--- exeptionstandart/exeptionstandart.cpp
+++ exeptionstandart/exeptionstandart.cpp
@@ -10,5 +10,15 @@ int main()
 	try {
 		array<int, 3> data = { 3, 5, 7 };
 		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
 	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
 }
