5b423726895ee4bd1ae0fbff78d34f84690baa3e
Amandaptr44 <amandaptr86@gmail.com>
2023-06-21 15:45:56 +0700

    mengeksekusi blok kode jika terjadi kesalahan

--- manualexeption/manualexeption.cpp
+++ manualexeption/manualexeption.cpp
@@ -6,6 +6,9 @@ int main()
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
 		throw 0.5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
 
 	}
 }
